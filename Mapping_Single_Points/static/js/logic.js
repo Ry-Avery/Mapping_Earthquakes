6c1c21432eb880790bd293cf2180ea8af5ed03e4
Ry Avery <axeguy255@gmail.com>
2022-04-26 16:48:17 -0700

    Added circleMarker to Los Angeles, California

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,11 +4,11 @@ console.log('Working');
 // Create the map object with a center and zoom level
 //let map = L.map('mapid').setView([40.7, -94.5], 4);
 let map = L.map('mapid',{
-    center: [40.7, -94.5],
-    zoom: 4
+    center: [34.0522, -118.2437],
+    zoom: 14
 });
 // We create the tile layer that will be the background of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributers, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery @ <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     tileSize: 512,
@@ -17,3 +17,11 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 // Then we add our 'greymap' tile layer to the map.
 streets.addTo(map);
+
+// Add a marker to the map for Los Angeles, California
+L.circleMarker([34.0522, -118.2437],{
+    radius: 300,
+    color: '#000000',
+    fillColor: '#ffffa1'
+}).addTo(map);
+
