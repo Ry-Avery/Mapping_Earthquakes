0c3d2aa177ab2d6bd1908925c2995baa0ff63c23
Ry Avery <axeguy255@gmail.com>
2022-04-26 17:35:49 -0700

    Mapped multiple circleMarker's
    Created an array in cities.js and used forEach to iterate through it and plot each marker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,8 +4,8 @@ console.log('Working');
 // Create the map object with a center and zoom level
 //let map = L.map('mapid').setView([40.7, -94.5], 4);
 let map = L.map('mapid',{
-    center: [34.0522, -118.2437],
-    zoom: 14
+    center: [40.7, -94.5],
+    zoom: 4
 });
 // We create the tile layer that will be the background of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
@@ -18,10 +18,15 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 // Then we add our 'greymap' tile layer to the map.
 streets.addTo(map);
 
-// Add a marker to the map for Los Angeles, California
-L.circleMarker([34.0522, -118.2437],{
-    radius: 300,
-    color: '#000000',
-    fillColor: '#ffffa1'
-}).addTo(map);
-
+// Get data from cities.js
+let cityData = cities;
+// Loop through the cities array and create one marker for each city
+cityData.forEach(function(city){
+    console.log(city)
+    L.circleMarker(city.location,{
+        radius: city.population/100000,
+        color: '#ffa500',
+    })
+    .bindPopup("<h2>" + city.city + ', ' + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
